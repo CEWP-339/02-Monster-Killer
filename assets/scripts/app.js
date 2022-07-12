1583e1eedfbb094eb9e94a733510e2236af107ad
Kritika Sharma <kritikasharma@Kritikas-MacBook-Pro.local>
2022-07-11 20:20:28 -0400

    implement attack functionality

---
+++ assets/scripts/app.js
@@ -0,0 +1,25 @@
+const ATTACK_VALUE = 10;
+const MONSTER_ATTACK_VALUE = 15;
+
+let chosenMaxLife = 50;
+let currentMonsterHealth = chosenMaxLife;
+let currentPlayerHealth = chosenMaxLife;
+
+adjustHealthBars(chosenMaxLife);
+
+function attackHandler () {
+    const damage = dealMonsterDamage(ATTACK_VALUE);
+    currentMonsterHealth -= damage;
+    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+    currentPlayerHealth -= playerDamage;
+
+    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+        alert('You won!');
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+        alert('You lost!');
+    } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
+        alert("It's a draw!");
+    }
+}
+
+attackBtn.addEventListener('click', attackHandler);
